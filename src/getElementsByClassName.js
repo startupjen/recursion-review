5e3bff7767bb874d2d2161869a69a20427e5825d
Hack Reactor Student <student@hackreactor.com>
2017-06-26 11:57:25 -0700

    Recursion Review V1 - put logic in, will be debugging

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,44 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ) {
-  // your code here
-};
+  var elementsWithClassName = [];
+
+  
+  //we'll start on document.body level
+  var documentBody = document.body;
+  var bodyClassList = documentBody.classList;
+
+
+  var findClassName = function(className,element) {
+    var classList = Array.prototype.slice.call(element.classList); 
+
+    for (var i = 0; i < classList.length; i++) {
+      if (classList[i] === className) {
+        elementsWithClassName.push(element);
+      }
+    }
+
+  //grab all the children nodes
+  //iterate through the children nodes and see if children nodes have classname
+  //push to array if it has it
+  //recursion
+
+    var children = element.children;
+    for (var i = 0; i < children.length; i++) {
+      findClassName(className,children[i]);
+    }
+  };
+  //element.classlist || element.children()
+
+  //if body level has classname
+
+  findClassName(className,documentBody);
+  
+
+//if classname found in element, push it to the result array
+  
+  
+
+
+  return elementsWithClassName; 
+  };
